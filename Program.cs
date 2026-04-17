d573897e131b5db8ddccdd2862144d69d121b0c4
zeyad tarek <vetogamer5@outlook.com>
2026-04-17 13:17:53 +0200

    Q10/Q11

--- Program.cs
+++ Program.cs
@@ -107,6 +107,13 @@
             //    TotalOrderValue = g.Sum(c => c.Orders.Sum(o => o.Total))
             //}; 
             #endregion
+            #region Q10
+            //int totalUnits = products.Sum(p => p.UnitsInStock); 
+            #endregion
+            #region Q11
+            //var minPrice = products.Min(p => p.UnitPrice);
+            //var maxPrice = products.Max(p => p.UnitPrice); 
+            #endregion
         }
     }
 }
