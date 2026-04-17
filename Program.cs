b9364b8dd3075afb3bd8ed8cd6e0ed40dd0f267a
zeyad tarek <vetogamer5@outlook.com>
2026-04-17 13:19:01 +0200

    Q14

--- Program.cs
+++ Program.cs
@@ -125,6 +125,13 @@
 
             //var resultSet = setA.Except(setB); 
             #endregion
+            #region Q14
+            //string[] list1 = { "Germany", "France", "UK", "Spain" };
+            //string[] list2 = { "france", "SPAIN", "Italy" };
+
+            //var resultCountries = list1
+            //    .Except(list2, StringComparer.OrdinalIgnoreCase); 
+            #endregion
         }
     }
 }
