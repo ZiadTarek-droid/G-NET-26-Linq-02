8712560e2d2e5d5bed9e2bbb6dccbe47e14a1ee6
zeyad tarek <vetogamer5@outlook.com>
2026-04-17 13:10:42 +0200

    Q5

--- Program.cs
+++ Program.cs
@@ -35,6 +35,11 @@
             //.Where(p => p.Category == "Seafood")
             //.All(p => p.UnitsInStock > 0); 
             #endregion
+            #region Q5
+            //int[] ids = { 3, 9, 13, 18 };
+
+            //bool contains9 = ids.Contains(9); 
+            #endregion
         }
     }
 }
